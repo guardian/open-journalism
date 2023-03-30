56b289e5971fd2c981619ee3674484fd4af61bcc
Max Duval <max.duval@theguardian.com>
2023-03-30 10:51:11 +0100

    feat: give report examples

--- src/main.js
+++ src/main.js
@@ -1,15 +1,45 @@
 import { get_report } from './report.js';
 
-const report = await get_report('230329_AiDcTT_8MB');
+const test = new URLSearchParams(window.location.search).get('test') ??
+	undefined;
 
-console.log(report);
+if (test) {
+	const report = await get_report(test);
 
-const ul = document.createElement('ul');
-document.body.appendChild(ul);
+	console.log(report);
 
-for (const request of report.requests) {
-	const li = document.createElement('li');
-	li.innerText = request.full_url + ' – ' + request.objectSize / 1000 + ' kB';
+	const ul = document.createElement('ul');
+	document.body.appendChild(ul);
 
-	ul.appendChild(li);
+	for (const request of report.requests) {
+		const li = document.createElement('li');
+		li.innerText = request.full_url + ' – ' + request.objectSize / 1000 + ' kB';
+
+		ul.appendChild(li);
+	}
+} else {
+	const p = document.createElement('p');
+	p.innerText =
+		'No test id provided, ensure it’s present in the url, for example:';
+	document.body.appendChild(p);
+
+	const ul = document.createElement('ul');
+	document.body.appendChild(ul);
+
+	const example_tests = /** @type {const} */ ([
+		'230117_BiDcKE_8T2',
+		'230324_AiDcQ1_9EZ',
+		'230329_AiDcTT_8MB',
+	]);
+
+	for (const test of example_tests) {
+		const li = document.createElement('li');
+		const a = document.createElement('a');
+		li.appendChild(a);
+
+		a.href = `?test=${test}`;
+		a.innerText = a.href;
+
+		ul.appendChild(li);
+	}
 }
