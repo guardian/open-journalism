963c94c7f83512dba1fba64808a11e9ca638241c
Max Duval <max.duval@theguardian.com>
2023-04-11 15:24:11 +0100

    fix: working Github Page
    Serve files from /open-journalism/ subfolder locally,
    to mimic production behaviour.

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import { serve } from 'https://deno.land/std@0.181.0/http/server.ts';
+import { serveDir } from 'https://deno.land/std@0.181.0/http/file_server.ts';
+import { fromFileUrl } from 'https://deno.land/std@0.182.0/path/mod.ts';
+
+const port = 4507;
+
+serve((req) => {
+	const url = new URL(req.url);
+
+	if (url.pathname.startsWith('/open-journalism/')) {
+		console.log(fromFileUrl(import.meta.resolve('./')));
+		return serveDir(req, {
+			fsRoot: fromFileUrl(import.meta.resolve('./')),
+			urlRoot: 'open-journalism',
+		});
+	} else if (url.searchParams.get('test')) {
+		return Response.redirect(
+			new URL(`/open-journalism/wpt/viewer/${url.search}`, url.origin),
+		);
+	} else {
+		return Response.redirect(new URL('/open-journalism/', url.origin));
+	}
+}, { port });
