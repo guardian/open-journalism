04253192b5a28470afedadff9b821e8e9f154d1b
Max Duval <max.duval@theguardian.com>
2023-05-16 17:05:56 +0100

    refactor: use camelCase for variable names

--- src/wpt/lib/get_image.test.ts
+++ src/wpt/lib/get_image.test.ts
@@ -1,16 +1,16 @@
 import { assertEquals } from 'https://deno.land/std@0.180.0/testing/asserts.ts';
-import { get_image_src } from './get_image.js';
+import { getImageSrc } from './get_image.js';
 
 Deno.test('returns a valid image url', () => {
 	assertEquals(
-		get_image_src('230117_BiDcKE_8T2'),
+		getImageSrc('230117_BiDcKE_8T2'),
 		'https://www.webpagetest.org/results/23/01/17/BiDcKE/8T2/1_screen.jpg',
 	);
 });
 
 Deno.test('returns undefined for an invalid test ID', () => {
 	assertEquals(
-		get_image_src('invalid_test_id'),
+		getImageSrc('invalid_test_id'),
 		undefined,
 	);
 });
